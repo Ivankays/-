d0244778570571b0f0a4e8219d5e67df44b27d32
Ivankays <144226977+Ivankays@users.noreply.github.com>
2023-10-27 10:28:36 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,17 @@
+﻿Program zad1;
+var x,y, flag:real;
+begin
+  write('введите значение ');
+  read(x);
+  if x<-6 then
+    y:=(tan(x)/17)*(tan(x/6));
+  if (-6<=x) and (x<=-1) then
+    y:=ln(x)*power(exp(x),x)+power(x,3)/power(exp(x),x);
+  if (-1<=x) then
+    y:=(tan(x)/cos(x))-(sin(x)/x*sqr(x));
+  if y=y then
+    flag:=1
+  else flag:=0;
+  if flag = 1 then write('x=',x,' ','y=',y:0:4) 
+              else write('x=',x,' ','Такого нет'); 
+  end.
---
+++ 3.pas
@@ -0,0 +1,20 @@
+﻿8Program zad2;
+var x,y,flag:real;
+begin
+  x:=-8; 
+  while x<=1 do 
+    begin
+  if x<-6 then
+    y:=(tan(x)/17)*(tan(x/6));
+  if (-6<=x) and (x<=-1) then
+    y:=ln(x)*power(exp(x),x)+power(x,3)/power(exp(x),x);
+  if (-1<=x) then
+    y:=(tan(x)/cos(x))-(sin(x)/x*sqr(x));
+  if y=y then
+    flag:=1
+  else flag:=0;
+  if flag = 1 then writeln('x=' , x:0:4 , ' ' , 'y=', y:0:4) 
+              else writeln('x=' , x:0:4 , ' ' , 'y=корней нет'); 
+    x:=x+0.2; 
+              end;
+end.
